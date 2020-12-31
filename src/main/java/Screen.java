f7fbe321f8e770bd74680ddf738b73896c036947
Sainath Bottupally <sai@Sainaths-MacBook-Pro.local>
2020-12-31 00:22:04 -0800

    Created travelling ball project

---
+++ src/main/java/Screen.java
@@ -0,0 +1,12 @@
+public class Screen {
+    private final int WIDTH = 640;
+    private final int HEIGHT = 480;
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+}
